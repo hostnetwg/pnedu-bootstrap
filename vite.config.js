bca468ac40d894678a21bb1d99326fe1f9e6787e
hostnetwg <hostnet.wg@gmail.com>
2025-09-14 16:00:22 +0200

    Zmiana konfiguracji localnie - zmiana portów

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,11 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5174,
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
