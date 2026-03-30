e4f7a062718f2813abde7a94e3797e442005fe20
hostnetwg <hostnet.wg@gmail.com>
2026-03-30 03:59:16 +0200

    Link do potwierdzenia akredytacji v1

--- resources/views/layouts/hero-banner.blade.php
+++ resources/views/layouts/hero-banner.blade.php
@@ -3,9 +3,15 @@
     <div class="container">
         <p class="lead fw-semibold mb-0">
             Niepubliczny Ośrodek Doskonalenia Nauczycieli "Platforma Nowoczesnej Edukacji"<br>
-            <span style="color: #ffd700; font-style: normal; font-weight: 600;">
+            <a href="{{ url('/'.rawurlencode('Decyzja MKO - akredytacja 5 lat (2025-2030).pdf')) }}"
+               class="text-decoration-underline"
+               style="color: #ffd700; font-style: normal; font-weight: 600;"
+               download
+               target="_blank"
+               rel="noopener noreferrer"
+               title="Pobierz potwierdzenie akredytacji (PDF)">
                 AKREDYTACJA MAZOWIECKIEGO KURATORA OŚWIATY
-            </span>
+            </a>
         </p>
     </div>
 </div>
